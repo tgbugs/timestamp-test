1d336a63ba9e1840575eae9fccf20c61917edab6
Tom Gillespie <tgbugs@gmail.com>
2018-01-15 16:42:59 -0800

    md needs spaces for newline

--- update.rkt
+++ update.rkt
@@ -6,7 +6,7 @@
 (define now (current-seconds))
 (define template "https://github.com/tgbugs/timestamp-test/blob/master@{~a}/test-file")
 (define (update-file)
-  (write-string (string-join (for/list ([i (range 100)]) (format template (+ now i))) "\n")))
+  (write-string (string-join (for/list ([i (range 100)]) (format template (+ now i))) "  \n")))
 (with-output-to-file "test-file.md" #:exists 'replace update-file)
 (define command (format
                  "date +%s && git add -u && git commit --date ~a-0800 -m 'test commit' && date +%s"
