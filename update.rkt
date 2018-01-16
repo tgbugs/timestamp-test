c32ae8b8f47417752cf40d2b643a6df8e1bd9f63
Tom Gillespie <tgbugs@gmail.com>
2018-01-15 16:40:40 -0800

    switched to md so that links are clickable

--- update.rkt
+++ update.rkt
@@ -7,7 +7,7 @@
 (define template "https://github.com/tgbugs/timestamp-test/blob/master@{~a}/test-file")
 (define (update-file)
   (write-string (string-join (for/list ([i (range 100)]) (format template (+ now i))) "\n")))
-(with-output-to-file "test-file" #:exists 'replace update-file)
+(with-output-to-file "test-file.md" #:exists 'replace update-file)
 (define command (format
                  "date +%s && git add -u && git commit --date ~a-0800 -m 'test commit' && date +%s"
                  now))
